eacc124548cfdb24663d1cd7efb8449996bb5532
Clive Walkden <clive@sozodesign.co.uk>
2016-11-25 11:54:05 +0000

    Added license to files

--- app/code/community/Sozo/Jivochat/Helper/Data.php
+++ app/code/community/Sozo/Jivochat/Helper/Data.php
@@ -1,15 +1,33 @@
 <?php
-class Sozo_Jivochat_Helper_Data extends Mage_Core_Helper_Abstract {
-	const XML_JIVOCHAT_ENABLED = 'sozo_jivochat/general/enabled';
-	const XML_JIVOCHAT_WIDGET_ID = 'sozo_jivochat/chatsetup/widget_id';
+/**
+ * SOZO Design
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category    SOZO Design
+ * @package     Sozo_Jivochat
+ * @copyright   Copyright (c) 2016 SOZO Design (http://www.sozodesign.com)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ *
+ */
 
-	public function isEnabled($store = null)
-	{
-		return Mage::getStoreConfigFlag(self::XML_JIVOCHAT_ENABLED, $store);
-	}
+class Sozo_Jivochat_Helper_Data extends Mage_Core_Helper_Abstract
+{
+    const XML_JIVOCHAT_ENABLED = 'sozo_jivochat/general/enabled';
+    const XML_JIVOCHAT_WIDGET_ID = 'sozo_jivochat/chatsetup/widget_id';
 
-	public function getWidgetId($store = null)
-	{
-		return Mage::getStoreConfig(self::XML_JIVOCHAT_WIDGET_ID, $store);
-	}
+    public function isEnabled($store = null)
+    {
+        return Mage::getStoreConfigFlag(self::XML_JIVOCHAT_ENABLED, $store);
+    }
+
+    public function getWidgetId($store = null)
+    {
+        return Mage::getStoreConfig(self::XML_JIVOCHAT_WIDGET_ID, $store);
+    }
 }
