450c2874e0dd52380a5b16ceb55b55f4b9ce4ae2
Clive Walkden <clive@sozodesign.co.uk>
2016-11-25 10:19:25 +0000

    Fixed helper xml link issue

--- app/code/community/Sozo/Jivochat/Helper/Data.php
+++ app/code/community/Sozo/Jivochat/Helper/Data.php
@@ -1,7 +1,7 @@
 <?php
 class Sozo_Jivochat_Helper_Data extends Mage_Core_Helper_Abstract {
 	const XML_JIVOCHAT_ENABLED = 'sozo_jivochat/general/enabled';
-	const XML_JIVOCHAT_WIDGET_ID = 'sozojivochat/chatsetup/widget_id';
+	const XML_JIVOCHAT_WIDGET_ID = 'sozo_jivochat/chatsetup/widget_id';
 
 	public function isEnabled($store = null)
 	{
